84c644d205576245130d45f0ab743979dd8bcbde
GamedevDeadend <tanmay10087@gmail.com>
2024-03-11 22:20:38 +0530

    Dynamic Linking

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@ int main()
 {
 	int a = glfwInit();
 
-	std::cout << "HelloWorld" << std::endl;
+	std::cout << "HelloWorld " <<a<< std::endl;
 	std::cin.get();
 }
+
+/*
+* for dynamic linking add dynamic lib of glfw3dll.lib in input of linker.
+* we can also use glfw3.dll but glfw3dll.lib is more optimised as it contains function locations by using function pointers
+*/
